c39a46ead7881e164b74fd6171997f3e789cd578
Ishara Shanuka <isharashanuka97@gmail.com>
2020-02-08 23:09:20 +0530

    Usestate with react native

--- myproject/App.js
+++ myproject/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [name, setname] = useState("Ishara");
+
+  const clickHanlder = () => {
+    setname("Janaka")
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Text style={styles.boldText}>My name is {name}</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="Click Here" onPress={clickHanlder} />
+      </View>
     </View>
   );
 }
@@ -12,8 +22,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  header: {
+    backgroundColor: "pink",
+    padding: 20
+  },
+  boldText: {
+    fontWeight: "bold"
+  },
+  background: {
+    backgroundColor: "yellow",
+    padding: 10,
+    textAlign: "justify"
+  },
+  buttonContainer: {
+    marginTop: 20
+  }
 });
